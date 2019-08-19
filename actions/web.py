1d79d0155a8733cbfd3cdad030f7ee85afcfe61c
Angaj <45209858+Angaj@users.noreply.github.com>
2019-08-19 12:45:36 +0530

    Update web.py

--- actions/web.py
+++ actions/web.py
@@ -2,7 +2,6 @@
 
 import sys
 import requests as req
-import webbrowser as web
 
 from st2common.runners.base_action import Action
 
@@ -10,6 +9,4 @@ class MyEchoAction(Action):
     def run(self, url):
 		resp = req.get(url)
 		print(resp.status_code)
-#		print(resp.history)
 		print(resp.url)
-		web.open_new_tab(url)
